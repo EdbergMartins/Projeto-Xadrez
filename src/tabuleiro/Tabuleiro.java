ca94b2d4824db961711304417aaf5c15af96cf3a
Edberg Nobrega <edbergmartins@gmail.com>
2020-04-24 14:58:26 -0300

    Excecoes do tabuleiro e programação defenciva

--- src/tabuleiro/Tabuleiro.java
+++ src/tabuleiro/Tabuleiro.java
@@ -6,8 +6,10 @@ public class Tabuleiro {
 	private Integer colunas;
 	private Pecas[][] pecas;
 	
-	
 	public Tabuleiro(Integer linhas, Integer colunas) {
+		if (linhas<1 || colunas <1) {
+			throw new ExcecoesTabuleiro("Erro ao criar o tabuleiro:  Necessário que haja mais de 1 linha e 1 coluna"); 
+		}
 		this.linhas = linhas;
 		this.colunas = colunas;
 		pecas = new Pecas[linhas][colunas];
@@ -16,30 +18,46 @@ public class Tabuleiro {
 	public Integer getLinhas() {
 		return linhas;
 	}
-	public void setLinhas(Integer linhas) {
-		this.linhas = linhas;
-	}
+
 	public Integer getColunas() {
 		return colunas;
 	}
-	public void setColunas(Integer colunas) {
-		this.colunas = colunas;
-	}
-	
+
 	public Pecas peca (int linha, int coluna) {
+		if (!posicaoExistentes(linha, coluna)) {
+			throw new ExcecoesTabuleiro("Posição fora do tabuleiro");
+		}
 		return pecas[linha][coluna];
-		
 	}
 	
 	public Pecas peca(Posicao posicao) {
+		if (!posisoesExistentes(posicao)) {
+			throw new ExcecoesTabuleiro("Posição fora do tabuleiro");
+		}
 		return pecas[posicao.getLinha()][posicao.getColuna()];
 	}
 
 	public void lugarPeca(Pecas peca, Posicao posicao) {
-		
+		if (existeUmaPeca(posicao)) {
+			throw new ExcecoesTabuleiro("Já existe uma na posicao: " + posicao);
+		}
 		pecas[posicao.getLinha()][posicao.getColuna()] = peca;
 		peca.posicao = posicao;
 	}
 	
+	private boolean posicaoExistentes(int linha, int coluna) {
+		return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas;
+	}
+	
+	public boolean posisoesExistentes(Posicao posicao) {
+		return posicaoExistentes(posicao.getLinha(),posicao.getColuna());
+	}
+	
+	public boolean existeUmaPeca (Posicao posicao) {
+		if (!posisoesExistentes(posicao)) {
+			throw new ExcecoesTabuleiro("Posição fora do tabuleiro");
+		}
+		return peca(posicao)!=null;		
+	}
 	
 }
