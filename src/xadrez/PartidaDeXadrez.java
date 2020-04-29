3050c7d75280c99cb77cfa54005a1004380643b6
Edberg Nobrega <edbergmartins@gmail.com>
2020-04-29 20:54:32 -0300

    Melhorias na impressão do tabuleiro

--- src/xadrez/PartidaDeXadrez.java
+++ src/xadrez/PartidaDeXadrez.java
@@ -2,7 +2,6 @@ package xadrez;
 
 
 
-import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
 import xadrez.pecas.Rei;
 import xadrez.pecas.Torre;
@@ -34,8 +33,18 @@ public class PartidaDeXadrez {
 	}
 	
 	private void configuracaoInicial() {
-		lugarNovaPeca('b', 6, new Torre(tabuleiro, Cor.BRANCO));
-		lugarNovaPeca('e', 8, new Rei(tabuleiro, Cor.PRETO));
-		lugarNovaPeca('e', 1, new Rei(tabuleiro, Cor.BRANCO));
+		lugarNovaPeca('c', 1, new Torre(tabuleiro, Cor.BRANCO));
+        lugarNovaPeca('c', 2, new Torre(tabuleiro, Cor.BRANCO));
+        lugarNovaPeca('d', 2, new Torre(tabuleiro, Cor.BRANCO));
+        lugarNovaPeca('e', 2, new Torre(tabuleiro, Cor.BRANCO));
+        lugarNovaPeca('e', 1, new Torre(tabuleiro, Cor.BRANCO));
+        lugarNovaPeca('d', 1, new Rei(tabuleiro, Cor.BRANCO));
+
+        lugarNovaPeca('c', 7, new Torre(tabuleiro, Cor.PRETO));
+        lugarNovaPeca('c', 8, new Torre(tabuleiro, Cor.PRETO));
+        lugarNovaPeca('d', 7, new Torre(tabuleiro, Cor.PRETO));
+        lugarNovaPeca('e', 7, new Torre(tabuleiro, Cor.PRETO));
+        lugarNovaPeca('e', 8, new Torre(tabuleiro, Cor.PRETO));
+        lugarNovaPeca('d', 8, new Rei(tabuleiro, Cor.PRETO));
 	}
 }
