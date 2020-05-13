59babdede9b0eb1286c711e58a28bd349b438f96
Edberg Nobrega <edbergmartins@gmail.com>
2020-05-13 11:39:29 -0300

    Tratamento de excecoes e limpando terminal

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import xadrez.ExcecoesXadrez;
 import xadrez.PartidaDeXadrez;
 import xadrez.PecaXadrez;
 import xadrez.PosicaoXadrez;
@@ -14,17 +16,29 @@ public class Program {
 		PartidaDeXadrez partida = new PartidaDeXadrez();
 		
 		while (true) {
-			Interface.imprimirTabuleiro(partida.getPecas());
-			System.out.println();
-			System.out.print("Origem: ");
-			PosicaoXadrez origem = Interface.lerPosicaoXadrez(sc);
-			
-			System.out.println();
-			System.out.print("Destino: ");
-			PosicaoXadrez destino = Interface.lerPosicaoXadrez(sc);
-			
-			PecaXadrez captuPecaXadrez = partida.performMovimentoXadrez(origem, destino);
-			
+			try {
+				Interface.clearScreen();
+				Interface.imprimirTabuleiro(partida.getPecas());
+				System.out.println();
+				System.out.print("Origem: ");
+				PosicaoXadrez origem = Interface.lerPosicaoXadrez(sc);
+				
+				System.out.println();
+				System.out.print("Destino: ");
+				PosicaoXadrez destino = Interface.lerPosicaoXadrez(sc);
+				
+				PecaXadrez captuPecaXadrez = partida.performMovimentoXadrez(origem, destino);
+			}
+			catch (ExcecoesXadrez e ) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch(InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+				
+				
+			}
 	}
 
 }
